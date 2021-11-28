caefda69e5c99bfe3245bb88fc82cc385958e429
SUCIU CODRIN <curiosul007@yahoo.com>
2021-11-28 20:31:41 +0200

    Initialize project

---
+++ src/main/java/com/codrin/pensiuneamea/PensiuneameaApplication.java
@@ -0,0 +1,13 @@
+package com.codrin.pensiuneamea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensiuneameaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensiuneameaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codrin/pensiuneamea/PensiuneameaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codrin.pensiuneamea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensiuneameaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
